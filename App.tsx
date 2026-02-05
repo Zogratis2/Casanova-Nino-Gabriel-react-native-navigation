eaac177b0af2b1376c404f673505ca895febb0d7
Niño Gabriel C. Casanova <gabrielcasanovaq@gmail.com>
2026-02-05 16:50:50 +0800

    Second Commit

--- App.tsx
+++ App.tsx
@@ -5,20 +5,18 @@ import { CartProvider } from './contexts/CartContext';
 import AppNavigator from './navigation/AppNavigator';
 import { Switch, View } from 'react-native';
 
-
 export default function App() {
-const [dark, setDark] = useState(false);
+  const [dark, setDark] = useState(false);
 
-
-return (
-<CartProvider>
-<NavigationContainer theme={dark ? DarkTheme : DefaultTheme}>
-<StatusBar style={dark ? 'light' : 'dark'} />
-<View style={{ position: 'absolute', top: 50, right: 20, zIndex: 10 }}>
-<Switch value={dark} onValueChange={() => setDark(!dark)} />
-</View>
-<AppNavigator />
-</NavigationContainer>
-</CartProvider>
-);
+  return (
+    <CartProvider>
+      <NavigationContainer theme={dark ? DarkTheme : DefaultTheme}>
+        <StatusBar style={dark ? 'light' : 'dark'} />
+        <View style={{ position: 'absolute', top: 50, right: 20, zIndex: 10 }}>
+          <Switch value={dark} onValueChange={() => setDark(!dark)} />
+        </View>
+        <AppNavigator />
+      </NavigationContainer>
+    </CartProvider>
+  );
 }
