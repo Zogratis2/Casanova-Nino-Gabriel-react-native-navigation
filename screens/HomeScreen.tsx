c286ade1b6db6dd76cb8c242302c78c6c3f3d812
Niño Gabriel C. Casanova <gabrielcasanovaq@gmail.com>
2026-02-06 22:19:18 +0800

    Price Update

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -6,16 +6,16 @@ import { getStyles, COLORS } from '../styles/GlobalStyles';
 import { useTheme } from '../App';
 
 const products: Product[] = [
-  { id: 1, name: 'Laptop', price: 1200, image: 'https://www.itech.ph/wp-content/uploads/2024/09/E1504FA-L11066WSM-450x577.jpg' },
-  { id: 2, name: 'Headphones', price: 150, image: 'https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_2.625,f_auto,h_214,q_auto,w_380/c_pad,h_214,w_380/R2013524-01?pgw=1' },
-  { id: 3, name: 'Keyboard', price: 280, image: 'https://beyondthebox.ph/cdn/shop/files/SM1MechanicalKeyboard_Light_-USDarkGray1_700x700.png?v=1721283771' },
-  { id: 4, name: 'Mouse', price: 250, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS2azMy-DoJaZdxQ5oIRVkHWN0wdwSKRM8Zg&s' },
-  { id: 5, name: 'Stuff Toy', price: 550, image: 'https://media.istockphoto.com/id/909772478/photo/brown-teddy-bear-isolated-in-front-of-a-white-background.jpg?s=612x612&w=0&k=20&c=F4252bOrMfRTB8kWm2oM2jlb9JXY08tKCaO5G_ms1Uw=' },
-  { id: 6, name: 'Jeans', price: 300, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWsSNxBCX-haK0M6vBE1sNeF79SF2QZx6JMg&s' },
-  { id: 7, name: 'Shirt', price: 200, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQreDwlXCvH_iOhlDQ_gfhHGcjqUqXF-my6lA&s' },
-  { id: 8, name: 'Sneakers', price: 450, image: 'https://n.nordstrommedia.com/it/8c46eb33-5167-46dd-93d7-80aeab1dfaf2.jpeg?h=368&w=240&dpr=2' },
-  { id: 9, name: 'Sling Bag', price: 350, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXbxeb1WOD4jI4k50VtIzUMa6F8U_R_nQMew&s' },
-  { id: 10, name: 'Jacket', price: 400, image: 'https://moncler-cdn.thron.com/api/v1/content-delivery/shares/dpx6uv/contents/L10911A00099597X2999_F/image/berre-short-down-jacket-men-black-moncler.jpg?w=600&q=80' },
+  { id: 1, name: 'Laptop', price: 11999, image: 'https://www.itech.ph/wp-content/uploads/2024/09/E1504FA-L11066WSM-450x577.jpg' },
+  { id: 2, name: 'Headphones', price: 1499, image: 'https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_2.625,f_auto,h_214,q_auto,w_380/c_pad,h_214,w_380/R2013524-01?pgw=1' },
+  { id: 3, name: 'Keyboard', price: 1000, image: 'https://beyondthebox.ph/cdn/shop/files/SM1MechanicalKeyboard_Light_-USDarkGray1_700x700.png?v=1721283771' },
+  { id: 4, name: 'Mouse', price: 749, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS2azMy-DoJaZdxQ5oIRVkHWN0wdwSKRM8Zg&s' },
+  { id: 5, name: 'Stuff Toy', price: 549, image: 'https://media.istockphoto.com/id/909772478/photo/brown-teddy-bear-isolated-in-front-of-a-white-background.jpg?s=612x612&w=0&k=20&c=F4252bOrMfRTB8kWm2oM2jlb9JXY08tKCaO5G_ms1Uw=' },
+  { id: 6, name: 'Jeans', price: 799, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWsSNxBCX-haK0M6vBE1sNeF79SF2QZx6JMg&s' },
+  { id: 7, name: 'Shirt', price: 499, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQreDwlXCvH_iOhlDQ_gfhHGcjqUqXF-my6lA&s' },
+  { id: 8, name: 'Sneakers', price: 3500, image: 'https://n.nordstrommedia.com/it/8c46eb33-5167-46dd-93d7-80aeab1dfaf2.jpeg?h=368&w=240&dpr=2' },
+  { id: 9, name: 'Sling Bag', price: 550, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXbxeb1WOD4jI4k50VtIzUMa6F8U_R_nQMew&s' },
+  { id: 10, name: 'Jacket', price: 399, image: 'https://moncler-cdn.thron.com/api/v1/content-delivery/shares/dpx6uv/contents/L10911A00099597X2999_F/image/berre-short-down-jacket-men-black-moncler.jpg?w=600&q=80' },
 ];
 
 export default function HomeScreen() {
